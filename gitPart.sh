5c5ac38951f14ead678e41696fcb2ca30323a710
red <kot514877@gmail.com>
2025-03-18 10:53:57 +0300

    first test commit done

---
+++ gitPart.sh
@@ -0,0 +1,12 @@
+git init git-part     #init git-repository
+
+cd git-part 
+
+
+#1
+cp -r ../commits/commit0/. ./src
+git checkout -b first_branch 
+git add .
+git commit -m 'red did r0' 
+echo 'red did r0' 
+ 
