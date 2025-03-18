780c5b096b878ab4840567fe9569b3763adfb5ad
red <kot514877@gmail.com>
2025-03-18 15:22:52 +0300

    added some commits

--- gitPart.sh
+++ gitPart.sh
@@ -3,10 +3,131 @@ git init git-part     #init git-repository
 cd git-part 
 
 
-#1
+# r0 red
+
+git config --global user.name "red"
+git config --global user.mail "red@itmo.ru"
 cp -r ../commits/commit0/. ./src
 git checkout -b first_branch 
 git add .
 git commit -m 'red did r0' 
 echo 'red did r0' 
- 
+
+# r1 blue
+
+
+
+
+
+# r2 red
+git config --global user.name "red"
+git config --global user.mail "red@itmo.ru"
+git checkout -b twetwentieth_branch
+cp -r ../commits/commit2/. ./src 
+git add . 
+git commit -m 'red did r2'
+echo 'red did r2'
+
+# r3 red 
+git config --global user.name "red"
+git config --global user.mail "red@itmo.ru"
+git checkout -b thirteenth_branch
+cp -r ../commits/commit3/. ./src 
+git add . 
+git commit -m 'red did r3' 
+echo 'red did r3'
+
+# r4 blue
+
+# r5 blue
+
+# r6 blue
+
+# r7 
+git config --global user.name "red"
+git config --global user.mail "red@itmo.ru"
+git checkout first_branch 
+cp -r ../commits/commit7/. ./src
+git add .
+git commit -m 'red did r7' 
+echo 'red did r7'
+
+# r8 blue
+
+# r9 red
+git config --global user.name "red"
+git config --global user.mail "red@itmo.ru"
+git checkout thirteenth_branch
+cp -r ../commits/commit9/. ./src 
+git add . 
+git commit -m 'red did r9' 
+echo 'red did r9'
+
+# r10 blue
+
+# r11 red
+git config --global user.name "red"
+git config --global user.mail "red@itmo.ru"
+git checkout thirteenth_branch
+cp -r ../commits/commit11/. ./src 
+git add . 
+git commit -m 'red did r11' 
+echo 'red did r11'
+
+# r12 blue
+
+# r13 red
+git config --global user.name "red"
+git config --global user.mail "red@itmo.ru"
+git checkout thirteenth_branch
+cp -r ../commits/commit13/. ./src
+git add . 
+git commit -m 'red did r13'
+echo 'red did r13'
+
+# r14 red
+git config --global user.name "red"
+git config --global user.mail "red@itmo.ru"
+git checkout first_branch 
+cp -r ../commits/commit14/. ./src
+git add . 
+git commit -m 'red did r14'
+echo 'red did r14' 
+
+# r15 blue
+
+# r16 blue
+
+# r17
+git config --global user.name "red"
+git config --global user.mail "red@itmo.ru"
+git checkout -b fifteenth_branch 
+cp -r ../commits/commit17/. ./src
+git add . 
+git commit -m 'red did r17'
+echo 'red did r17'
+
+# r18 blue
+
+# r19 blue
+
+# r20 blue
+
+# r21 blue
+
+# r22 blue
+
+# r23 blue
+
+# r24 blue 
+
+# r25 red
+git config --global user.name "red"
+git config --global user.mail "red@itmo.ru"
+git checkout thirteenth_branch
+cp -r ../commits/commit25/. ./src
+git add .
+git commit -m 'red did r25'
+echo 'red did r25'
+
+
