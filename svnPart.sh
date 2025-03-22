9d5b95f3c58150a98b14fd7407b57c580d74696d
vodka_kota <109616806+DopleZZ@users.noreply.github.com>
2025-03-22 18:59:58 +0300

    blue svn part

--- svnPart.sh
+++ svnPart.sh
@@ -19,8 +19,15 @@ svn commit -m "red did r0" --username "red"
 
 
 # r1 blue
+svn copy $RepoPath/trunk $RepoPath/branches/second_branch -m "Created/second_branch"
+svn switch $RepoPath/branches/second_branch
+svn rm *
+cp -r ../commits/commit1/. ./
+svn add *
+svn commit -m "blue did r1" --username "blue"
 
-svn copy $RepoPath/branches/first_branch $RepoPath/branches/seventeenth_branch -m "Created first_branch"
+
+svn copy $RepoPath/branches/first_branch $RepoPath/branches/seventeenth_branch -m "Created first_branch" #???????????????????????????
 
 # r2 red
 svn copy $RepoPath/branches/seventeenth_branch $RepoPath/branches/twentieth_branch -m "Created twentieth_branch"
@@ -40,10 +47,26 @@ svn commit -m "red did r3" --username "red"
 
 
 # r4 blue
+svn copy $RepoPath/trunk $RepoPath/branches/third_branch -m "Created third_branch"
+svn switch $RepoPath/branches/third_branch
+svn rm *
+cp -r ../commits/commit4/. ./
+svn add *
+svn commit -m "blue did r4" --username "blue"
 
 # r5 blue
+svn copy $RepoPath/trunk $RepoPath/branches/fourteenth_branch -m "Created fourteenth_branch"
+svn switch $RepoPath/branches/fourteenth_branch
+svn rm *
+cp -r ../commits/commit5/. ./
+svn add *
+svn commit -m "blue did r5" --username "blue"
 
 # r6 blue
+svn rm *
+cp -r ../commits/commit6/. ./
+svn add *
+svn commit -m "blue did r6" --username "blue"
 
 # r7 red
 svn switch $RepoPath/branches/first_branch
@@ -53,6 +76,12 @@ svn add *
 svn commit -m "red did r7" --username "red"
 
 # r8 blue
+svn copy $RepoPath/trunk $RepoPath/branches/eighteenth_branch -m "Created eighteenth_branch"
+svn switch $RepoPath/branches/eighteenth_branch
+svn rm *
+cp -r ../commits/commit8/. ./
+svn add *
+svn commit -m "blue did r8" --username "blue"
 
 # r9 red
 svn switch $RepoPath/branches/thirteenth_branch
@@ -62,6 +91,12 @@ svn add *
 svn commit -m "red did r9" --username "red"
 
 # r10 blue
+svn copy $RepoPath/trunk $RepoPath/branches/fifth_branch -m "Created fifth_branch"
+svn switch $RepoPath/branches/fifth_branch
+svn rm *
+cp -r ../commits/commit10/. ./
+svn add *
+svn commit -m "blue did r10" --username "blue"
 
 # r11 red
 svn switch $RepoPath/branches/thirteenth_branch
@@ -71,6 +106,12 @@ svn add *
 svn commit -m "red did r11" --username "red"
 
 # r12 blue
+svn copy $RepoPath/trunk $RepoPath/branches/second_branch -m "Created second_branch"
+svn switch $RepoPath/branches/second_branch
+svn rm *
+cp -r ../commits/commit12/. ./
+svn add *
+svn commit -m "blue did r12" --username "blue"
 
 # r13 red
 svn switch $RepoPath/branches/thirteenth_branch
@@ -87,8 +128,20 @@ svn add *
 svn commit -m "red did r14" --username "red"
 
 # r15 blue
+svn copy $RepoPath/trunk $RepoPath/branches/fourth_branch -m "Created fourth_branch"
+svn switch $RepoPath/branches/fourth_branch
+svn rm *
+cp -r ../commits/commit15/. ./
+svn add *
+svn commit -m "blue did r15" --username "blue"
 
 # r16 blue
+svn copy $RepoPath/trunk $RepoPath/branches/ninth_branch -m "Created ninth_branch"
+svn switch $RepoPath/branches/ninth_branch
+svn rm *
+cp -r ../commits/commit16/. ./
+svn add *
+svn commit -m "blue did r16" --username "blue"
 
 # r17 red
 svn copy $RepoPath/branches/ninth_branch $RepoPath/branches/fifteenth_branch -m "Created fifteenth_branch"
@@ -96,21 +149,57 @@ svn switch $RepoPath/branches/fifteenth_branch
 svn rm *
 cp -r ../commits/commit7/. ./
 svn add *
-svn commit -m "red did r7" --username "red"
+svn commit -m "red did r17" --username "red"
 
 # r18 blue
+svn copy $RepoPath/trunk $RepoPath/branches/tenth_branch -m "Created tenth_branch"
+svn switch $RepoPath/branches/tenth_branch
+svn rm *
+cp -r ../commits/commit18/. ./
+svn add *
+svn commit -m "blue did r18" --username "blue"
 
 # r19 blue
+svn switch $RepoPath/branches/ninth_branch
+svn rm *
+cp -r ../commits/commit19/. ./
+svn add *
+svn commit -m "blue did r19" --username "blue"
 
 # r20 blue
+svn switch $RepoPath/branches/eighteenth_branch
+svn rm *
+cp -r ../commits/commit20/. ./
+svn add *
+svn commit -m "blue did r20" --username "blue"
 
 # r21 blue
+svn copy $RepoPath/trunk $RepoPath/branches/sixth_branch -m "Created sixth_branch"
+svn switch $RepoPath/branches/sixth_branch
+svn rm *
+cp -r ../commits/commit21/. ./
+svn add *
+svn commit -m "blue did r21" --username "blue"
 
 # r22 blue
+svn rm *
+cp -r ../commits/commit22/. ./
+svn add *
+svn commit -m "blue did r22" --username "blue"
 
 # r23 blue
+svn switch $RepoPath/branches/sixteenth_branch
+svn rm *
+cp -r ../commits/commit23/. ./
+svn add *
+svn commit -m "blue did r23" --username "blue"
 
 # r24 blue
+svn switch $RepoPath/branches/tenth_branch
+svn rm *
+cp -r ../commits/commit24/. ./
+svn add *
+svn commit -m "blue did r24" --username "blue"
 
 # r25 red
 svn switch $RepoPath/branches/thirteenth_branch
@@ -128,7 +217,11 @@ svn add *
 svn commit -m "red did r26" --username "red"
 
 # r27 blue
-
+svn switch $RepoPath/branches/fourteenth_branch
+svn rm *
+cp -r ../commits/commit27/. ./
+svn add *
+svn commit -m "blue did r27" --username "blue"
 
 # r28 red
 svn copy $RepoPath/branches/fourteenth_branch $RepoPath/branches/eleventh_branch -m "Created eleventh_branch"
@@ -153,12 +246,32 @@ svn add *
 svn commit -m "red did r30" --username "red"
 
 # r31 blue
+svn switch $RepoPath/branches/second_branch
+svn rm *
+cp -r ../commits/commit31/. ./
+svn add *
+svn commit -m "blue did r31" --username "blue"
 
 # r32 blue
+svn switch $RepoPath/branches/sixth_branch
+svn rm *
+cp -r ../commits/commit32/. ./
+svn add *
+svn commit -m "blue did r32" --username "blue"
 
 # r33 blue
+svn rm *
+cp -r ../commits/commit33/. ./
+svn add *
+svn commit -m "blue did r33" --username "blue"
 
 # r34 blue
+svn copy $RepoPath/trunk $RepoPath/branches/twelfth_branch -m "Created twelfth_branch"
+svn switch $RepoPath/branches/twelfth_branch
+svn rm *
+cp -r ../commits/commit34/. ./
+svn add *
+svn commit -m "blue did r34" --username "blue"
 
 # r35 red
 svn switch $RepoPath/branches/twentieth_branch
@@ -189,6 +302,11 @@ svn add *
 svn commit -m "red did r38" --username "red"
 
 # r39 blue
+svn switch $RepoPath/branches/fifth_branch
+svn rm *
+cp -r ../commits/commit39/. ./
+svn add *
+svn commit -m "blue did r39" --username "blue"
 
 # r40 red
 svn switch $RepoPath/branches/eleventh_branch
@@ -198,12 +316,33 @@ svn add *
 svn commit -m "red did r40" --username "red"
 
 # r41 blue
+svn switch $RepoPath/branches/sixth_branch
+svn rm *
+cp -r ../commits/commit41/. ./
+svn add *
+svn commit -m "blue did r41" --username "blue"
 
 # r42 blue
+svn rm *
+cp -r ../commits/commit42/. ./
+svn add *
+svn commit -m "blue did r42" --username "blue"
 
 # r43 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/fourth_branch
+svn rm *
+svn merge $RepoPath/branches/sixth_branch $RepoPath/branches/fourth_branch
+svn resolve --action postpone
+cp -r ../commits/commit43/. ./
+svn add *
+svn commit -m "blue did r43" --username "blue"
 
 # r44 blue
+svn switch $RepoPath/branches/fourteenth_branch
+svn rm *
+cp -r ../commits/commit44/. ./
+svn add *
+svn commit -m "blue did r44" --username "blue"
 
 # r45 red
 svn switch $RepoPath/branches/seventh_branch
@@ -213,8 +352,20 @@ svn add *
 svn commit -m "red did r45" --username "red"
 
 # r46 blue
+svn switch $RepoPath/branches/twelfth_branch
+svn rm *
+cp -r ../commits/commit46/. ./
+svn add *
+svn commit -m "blue did r46" --username "blue"
 
 # r47 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/tenth_branch
+svn rm *
+svn merge $RepoPath/branches/twelfth_branch $RepoPath/branches/tenth_branch
+svn resolve --action postpone
+cp -r ../commits/commit47/. ./
+svn add *
+svn commit -m "blue did r47" --username "blue"
 
 # r48 red
 svn switch $RepoPath/branches/seventh_branch
@@ -231,6 +382,11 @@ svn add *
 svn commit -m "red did r49" --username "red"
 
 # r50 blue
+svn switch $RepoPath/branches/fourteenth_branch
+svn rm *
+cp -r ../commits/commit50/. ./
+svn add *
+svn commit -m "blue did r50" --username "blue"
 
 # r51 red
 svn switch $RepoPath/branches/twentieth_branch
@@ -240,8 +396,20 @@ svn add *
 svn commit -m "red did r51" --username "red"
 
 # r52 blue
+svn switch $RepoPath/branches/seventeenth_branch
+svn rm *
+cp -r ../commits/commit52/. ./
+svn add *
+svn commit -m "blue did r52" --username "blue"
 
 # r53 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/fourteenth_branch
+svn rm *
+svn merge $RepoPath/branches/seventeenth_branch $RepoPath/branches/fourteenth_branch
+svn resolve --action postpone
+cp -r ../commits/commit53/. ./
+svn add *
+svn commit -m "blue did r53" --username "blue"
 
 # r54 red
 svn switch $RepoPath/branches/eleventh_branch
@@ -258,6 +426,12 @@ svn add *
 svn commit -m "red did r55" --username "red"
 
 # r56 blue
+svn copy $RepoPath/trunk $RepoPath/branches/nineteenth_branch -m "Created nineteenth_branch"
+svn switch $RepoPath/branches/nineteenth_branch
+svn rm *
+cp -r ../commits/commit56/. ./
+svn add *
+svn commit -m "blue did r56" --username "blue"
 
 # r57 red
 svn switch $RepoPath/branches/twentieth_branch
@@ -267,12 +441,35 @@ svn add *
 svn commit -m "red did r57" --username "red"
 
 # r58 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/tenth_branch
+svn rm *
+svn merge $RepoPath/branches/twentieth_branch $RepoPath/branches/tenth_branch
+svn resolve --action postpone
+cp -r ../commits/commit58/. ./
+svn add *
+svn commit -m "blue did r58" --username "blue"
 
 # r59 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/fourteenth_branch
+svn rm *
+svn merge $RepoPath/branches/tenth_branch $RepoPath/branches/fourteenth_branch
+svn resolve --action postpone
+cp -r ../commits/commit59/. ./
+svn add *
+svn commit -m "blue did r59" --username "blue"
 
 # r60 blue
+svn switch $RepoPath/branches/fourth_branch
+svn rm *
+cp -r ../commits/commit60/. ./
+svn add *
+svn commit -m "blue did r60" --username "blue"
 
 # r61 blue
+svn rm *
+cp -r ../commits/commit61/. ./
+svn add *
+svn commit -m "blue did r61" --username "blue"
 
 # r62 red
 svn switch $RepoPath/branches/fifteenth_branch
@@ -295,6 +492,11 @@ svn commit -m "red did merge in 63" --username "red"
 svn log #чета посмотрим как он мердж сделал или нет
 
 # r64 blue
+svn switch $RepoPath/branches/eighteenth_branch
+svn rm *
+cp -r ../commits/commit64/. ./
+svn add *
+svn commit -m "blue did r64" --username "blue"
 
 # r65 red
 svn switch $RepoPath/branches/seventh_branch
@@ -304,6 +506,11 @@ svn add *
 svn commit -m "red did r65" --username "red"
 
 # r66 blue
+svn switch $RepoPath/branches/fifth_branch
+svn rm *
+cp -r ../commits/commit66/. ./
+svn add *
+svn commit -m "blue did r66" --username "blue"
 
 # r67 red
 svn switch $RepoPath/branches/first_branch
@@ -313,8 +520,20 @@ svn add *
 svn commit -m "red did r67" --username "red"
 
 # r68 blue
+svn switch $RepoPath/branches/fourteenth_branch
+svn rm *
+cp -r ../commits/commit68/. ./
+svn add *
+svn commit -m "blue did r68" --username "blue"
 
 # r69 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/eighteenth_branch
+svn rm *
+svn merge $RepoPath/branches/fourteenth_branch $RepoPath/branches/eighteenth_branch
+svn resolve --action postpone
+cp -r ../commits/commit69/. ./
+svn add *
+svn commit -m "blue did r69" --username "blue"
 
 
 # r70 red
@@ -329,12 +548,23 @@ svn switch $RepoPath/branches/seventh_branch
 svn rm *
 cp -r ../commits/commit71/. ./
 svn add *
-svn commit -m "red did r71" --username "red"ß
+svn commit -m "red did r71" --username "red"
 
 # r72 blue DO MERGE 🥹
-
+svn switch  $RepoPath/branches/fifth_branch
+svn rm *
+svn merge $RepoPath/branches/seventh_branch $RepoPath/branches/fifth_branch
+svn resolve --action postpone
+cp -r ../commits/commit72/. ./
+svn add *
+svn commit -m "blue did r72" --username "blue"
 
 # r73 blue
+svn switch $RepoPath/branches/fourth_branch
+svn rm *
+cp -r ../commits/commit73/. ./
+svn add *
+svn commit -m "blue did r73" --username "blue"
 
 # r74 red
 svn switch $RepoPath/branches/first_branch
@@ -344,6 +574,11 @@ svn add *
 svn commit -m "red did r74" --username "red"
 
 # r75 blue
+svn switch $RepoPath/branches/third_branch
+svn rm *
+cp -r ../commits/commit75/. ./
+svn add *
+svn commit -m "blue did r75" --username "blue"
 
 # r76 red
 svn switch  $RepoPath/branches/thirteenth_branch
@@ -356,12 +591,36 @@ svn commit -m "red did r76" --username "red"
 
 
 # r77 blue
+svn switch $RepoPath/branches/seventeenth_branch
+svn rm *
+cp -r ../commits/commit77/. ./
+svn add *
+svn commit -m "blue did r77" --username "blue"
 
 # r78 blue
+svn switch $RepoPath/branches/second_branch
+svn rm *
+cp -r ../commits/commit78/. ./
+svn add *
+svn commit -m "blue did r78" --username "blue"
 
 # r79 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/fifteenth_branch
+svn rm *
+svn merge $RepoPath/branches/second_branch $RepoPath/branches/fifteenth_branch
+svn resolve --action postpone
+cp -r ../commits/commit79/. ./
+svn add *
+svn commit -m "blue did r79" --username "blue"
 
 # r80 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/eighteenth_branch
+svn rm *
+svn merge $RepoPath/branches/fifteenth_branch $RepoPath/branches/eighteenth_branch
+svn resolve --action postpone
+cp -r ../commits/commit80/. ./
+svn add *
+svn commit -m "blue did r80" --username "blue"
 
 # r81 red
 svn switch  $RepoPath/branches/first_branch
@@ -381,6 +640,11 @@ svn add *
 svn commit -m "red did r82" --username "red"
 
 # r83 blue
+svn switch $RepoPath/branches/nineteenth_branch
+svn rm *
+cp -r ../commits/commit83/. ./
+svn add *
+svn commit -m "blue did r83" --username "blue"
 
 # r84 red
 svn switch $RepoPath/branches/eighth_branch
@@ -406,12 +670,38 @@ svn add *
 svn commit -m "red did r86" --username "red"
 
 # r87 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/fifth_branch
+svn rm *
+svn merge $RepoPath/branches/thirteenth_branch $RepoPath/branches/fifth_branch
+svn resolve --action postpone
+cp -r ../commits/commit87/. ./
+svn add *
+svn commit -m "blue did r87" --username "blue"
 
 # r88 blue
+svn switch $RepoPath/branches/nineteenth_branch
+svn rm *
+cp -r ../commits/commit88/. ./
+svn add *
+svn commit -m "blue did r88" --username "blue"
 
 # r89 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/ninth_branch
+svn rm *
+svn merge $RepoPath/branches/nineteenth_branch $RepoPath/branches/ninth_branch
+svn resolve --action postpone
+cp -r ../commits/commit89/. ./
+svn add *
+svn commit -m "blue did r89" --username "blue"
 
 # r90 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/fourth_branch
+svn rm *
+svn merge $RepoPath/branches/ninth_branch $RepoPath/branches/fourth_branch
+svn resolve --action postpone
+cp -r ../commits/commit90/. ./
+svn add *
+svn commit -m "blue did r90" --username "blue"
 
 # r91 red
 svn switch  $RepoPath/branches/eleventh_branch
@@ -429,8 +719,20 @@ svn add *
 svn commit -m "red did r92" --username "red"
 
 # r93 blue DO MERGE 🥹
+svn switch  $RepoPath/branches/fifth_branch
+svn rm *
+svn merge $RepoPath/branches/eleventh_branch $RepoPath/branches/fifth_branch
+svn resolve --action postpone
+cp -r ../commits/commit93/. ./
+svn add *
+svn commit -m "blue did r93" --username "blue"
 
 # r94 blue
+svn switch $RepoPath/branches/nineteenth_branch
+svn rm *
+cp -r ../commits/commit94/. ./
+svn add *
+svn commit -m "blue did r94" --username "blue"
 
 # r95
 svn switch  $RepoPath/branches/first_branch
